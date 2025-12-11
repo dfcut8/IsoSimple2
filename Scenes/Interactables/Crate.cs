44a8042e8147da69a5ce7bc95eb95747235362c5
dfcut8 <dfcut8@proton.me>
2025-12-11 09:53:59 -0500

    add code to crate

---
+++ Scenes/Interactables/Crate.cs
@@ -0,0 +1,34 @@
+using Godot;
+
+namespace Scenes.Interactables;
+
+public partial class Crate : StaticBody2D
+{
+    private Area2D hitArea;
+    private AnimationPlayer animationPlayer;
+
+    public override void _Ready()
+    {
+        hitArea = GetNode<Area2D>("%HitArea");
+        animationPlayer = GetNode<AnimationPlayer>("%AnimationPlayer");
+
+        hitArea.AreaEntered += OnAreaEntered;
+    }
+
+    private void OnAreaEntered(Area2D other)
+    {
+        if (other.IsInGroup("Weapon"))
+        {
+            animationPlayer.Play("Destroyed");
+            animationPlayer.AnimationFinished += OnDestroyAnimationFinished;
+        }
+    }
+
+    private void OnDestroyAnimationFinished(StringName animName)
+    {
+        if (!string.IsNullOrEmpty(animName) && animName == "Destroyed")
+        {
+            QueueFree();
+        }
+    }
+}
