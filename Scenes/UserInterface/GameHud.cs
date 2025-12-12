f0ba365bea736bc796087f4d016a207d3402c871
dfcut8 <dfcut8@proton.me>
2025-12-12 10:23:55 -0500

    direct approach to render lives

---
+++ Scenes/UserInterface/GameHud.cs
@@ -0,0 +1,36 @@
+using System.Linq;
+using Godot;
+using Scenes.Player;
+
+namespace Scenes.UserInterface;
+
+public partial class GameHud : CanvasLayer
+{
+    private readonly int heartsRowSize = 8;
+    private readonly int heartsOffset = 16;
+    private Sprite2D heartsSprite;
+
+    public override void _Ready()
+    {
+        heartsSprite = GetNode<Sprite2D>("%Hearts");
+        for (int i = 0; i < PlayerState.CurrentLives; i++)
+        {
+            var sprite = new Sprite2D();
+            sprite.Texture = heartsSprite.Texture;
+            sprite.Hframes = heartsSprite.Hframes;
+            heartsSprite.AddChild(sprite);
+        }
+    }
+
+    public override void _Process(double delta)
+    {
+        foreach (var heart in heartsSprite.GetChildren().Cast<Sprite2D>())
+        {
+            var i = heart.GetIndex();
+            var x = i % heartsRowSize * heartsOffset;
+            var y = i / heartsRowSize * heartsOffset;
+            GD.Print($"Add sprite at {x}, {y}");
+            heart.Position = new Vector2(x, y);
+        }
+    }
+}
