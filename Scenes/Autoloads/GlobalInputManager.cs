6c84989d5e3b8a57968fb909169dc9d77ade7ffb
dfcut8 <dfcut8@proton.me>
2025-12-12 09:30:03 -0500

    fixed issue if we enter area in crate twice

---
+++ Scenes/Autoloads/GlobalInputManager.cs
@@ -0,0 +1,23 @@
+using Godot;
+
+namespace Scenes.Autoloads;
+
+public partial class GlobalInputManager : Node
+{
+    public const string GLOBAL_INPUT_STAGE_RESET = "global_stage_reset";
+
+    public static GlobalInputManager Instance { get; private set; }
+
+    public GlobalInputManager()
+    {
+        Instance = this;
+    }
+
+    public override void _Process(double delta)
+    {
+        if (Input.IsActionJustPressed(GLOBAL_INPUT_STAGE_RESET))
+        {
+            GetTree().ReloadCurrentScene();
+        }
+    }
+}
--- Scenes/Interactables/Crate.cs
+++ Scenes/Interactables/Crate.cs
@@ -6,6 +6,13 @@ public partial class Crate : StaticBody2D
 {
     private Area2D hitArea;
     private AnimationPlayer animationPlayer;
+    private CrateState currentState = CrateState.Exists;
+
+    private enum CrateState
+    {
+        Exists,
+        Destroying,
+    }
 
     public override void _Ready()
     {
@@ -18,8 +25,13 @@ public partial class Crate : StaticBody2D
     private void OnAreaEntered(Area2D other)
     {
         GD.Print($"AreaEntered. Other: {other.Name}");
+        if (currentState == CrateState.Destroying)
+        {
+            return;
+        }
         animationPlayer.Play("Destroyed");
         animationPlayer.AnimationFinished += OnDestroyAnimationFinished;
+        currentState = CrateState.Destroying;
     }
 
     private void OnDestroyAnimationFinished(StringName animName)
