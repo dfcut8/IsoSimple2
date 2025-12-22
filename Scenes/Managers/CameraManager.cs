7e90dd1f9a5ee4191ecbad0dc372d50ce26977fc
dfcut8 <dfcut8@proton.me>
2025-12-22 18:25:36 -0500

    still debugging map size

--- Scenes/Managers/CameraManager.cs
+++ Scenes/Managers/CameraManager.cs
@@ -13,16 +13,20 @@ public partial class CameraManager : Node2D
         // Fix for CS0019: Operator '*' cannot be applied to operands of type 'Rect2I' and 'Vector2I'
         // Instead, calculate the map size by multiplying the size of the used rect by the tile size.
         var usedRect = groundTileMap.GetUsedRect();
-        var tileSize = groundTileMap.TileSet.TileSize;
-        var mapSize = new Rect2I(
-            usedRect.Position * tileSize,
-            usedRect.Size * tileSize
-        );
-        followCamera.LimitEnabled = false;
-        followCamera.LimitBottom = (mapSize.End.Y);
-        followCamera.LimitRight = (mapSize.End.X);
-        followCamera.LimitTop = (mapSize.Position.Y);
-        followCamera.LimitLeft = (mapSize.Position.X);
+        var topLeft = groundTileMap.MapToLocal(usedRect.Position);
+        GD.Print(topLeft); // (112, -8)
+        var bottomRight = groundTileMap.MapToLocal(usedRect.End);
+        GD.Print(bottomRight); // (48, 152)
+        var size = groundTileMap.MapToLocal(usedRect.Size);
+        GD.Print(size); // (-48, 168)
+
+
+        followCamera.LimitEnabled = true;
+        followCamera.LimitBottom = (int)size.Y;
+        followCamera.LimitRight = (int)size.X;
+        followCamera.LimitTop = (int)topLeft.Y;
+        //followCamera.LimitLeft = -((int)topLeft.X + groundTileMap.TileSet.TileSize.X);
+        followCamera.LimitLeft = -(int)topLeft.X / 2;
         CharacterBody2D player = GetTree().GetNodesInGroup("Player").Cast<CharacterBody2D>().FirstOrDefault();
         player.AddChild(followCamera);
     }
